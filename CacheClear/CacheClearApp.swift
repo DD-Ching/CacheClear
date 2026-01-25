53e19568a0d07625278a55d21e00f4397a90a30a
DDH <bryanm513kj@gmail.com>
2026-01-25 11:22:28 +0800

    Initial Commit

---
+++ CacheClear/CacheClearApp.swift
@@ -0,0 +1,17 @@
+//
+//  CacheClearApp.swift
+//  CacheClear
+//
+//  Created by 沒問題的 on 2026/1/25.
+//
+
+import SwiftUI
+
+@main
+struct CacheClearApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
